c09f8ff52bb547a8b20847bb5941eed26994896b
yt <lucas_matias@protonmail.com>
2025-05-05 23:26:15 -0300

    Atualizando python estruturado

--- python-estruturado/estudo_listas.py
+++ python-estruturado/estudo_listas.py
@@ -3,10 +3,21 @@
 '''
 
 # criando lista
-lista1 = []
-lista2 = list()
+lista1 = [] # lista vazia
+lista2 = list() # lista vazia
+lista3 = [1,2,3] # lista com números
+print(lista1)
+print(lista2)
+print(lista3)
 
 # .append(): Adicionar elementos no final da lista
 carros = []
 carros.append('fusca')
 carros.append('Jipe')
+print(carros)
+
+# .insert(): Inserir um dado em determinada posição da lista baseado no índice
+nomes = ['Carla', 'Mateus', 'João']
+print(nomes)
+nomes.insert(1, 'Daniela')
+print(nomes)
