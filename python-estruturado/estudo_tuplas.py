8b2759dbb74f33c299c97baa5e961a43ff1d5f56
yt <lucas_matias@protonmail.com>
2025-05-09 04:48:25 -0300

    Atualizando python estruturado: tuplas

--- python-estruturado/estudo_tuplas.py
+++ python-estruturado/estudo_tuplas.py
@@ -2,11 +2,13 @@
     Estudo tuplas
 '''
 
-# tuplas: Coleções ordenadas, imutáveis, que permitem valores duplicados, e iterável
+# tuplas: Coleções ordenadas, imutáveis, heterogênea, que permitem valores duplicados e iteráveis
 
 ## criando tupla
 frutas = ('banana', 'maçã', 'pera')
 print(frutas) 
+nomes = tuple(('Daniele', 'Carlos', 'Pam'))
+print(nomes, type(nomes))
 
 ## criando tupla com apenas 1 item (obrigatório colocar uma vírgula)
 carros = ('van',)
@@ -18,3 +20,31 @@ print(len(frutas))
 ## type(tupla): verificar tipo do elemento
 print(type(carros))
 
+## .count(value): contar quantidade de determinado elemento
+pessoas = tuple(('Daniel', 'Carlos', 'Maria', 'Daniel', 'Santos'))
+print(f'tupla: {pessoas}; Tipos {type(pessoas)}' )
+print('Quantidade de "Daniel: {}'.format(pessoas.count('Daniel')))
+
+## .index(valor, inicio, fim): retornar o índice de um ocorrência. 
+pessoas = tuple(('Daniel', 'Carlos', 'Maria', 'Daniel', 'Santos', 'Daniel'))
+print(f'tupla: {pessoas}; Tipos {type(pessoas)}' )
+print('Ocorrência de "Daniel: {}'.format(pessoas.index('Daniel',4)))
+
+## "elemento" in tuple: Verificando se há um elemento dentro da tupla. Retorna True ou False
+print( 'Carlo' in pessoas)
+
+# Operações com tupla
+carros = ('Fiat', 'Chevrolet', 'Fiat Uno')
+motos = ('Honda', 'Pop 100')
+
+## Desempacotamento
+c1, c2, c3 = carros
+print(c1, c2, c3)
+
+## Concatenação
+veiculos = carros + motos
+print(veiculos)
+
+## Repetição
+motos2 = motos * 3
+print(motos2)
