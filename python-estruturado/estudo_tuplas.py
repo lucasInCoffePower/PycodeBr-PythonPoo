1fcb5765bd595f5f0cd6d0a13a739aa8a4131605
yt <lucas_matias@protonmail.com>
2025-05-08 23:56:25 -0300

    Atualizando README.md python estruturado

--- python-estruturado/estudo_tuplas.py
+++ python-estruturado/estudo_tuplas.py
@@ -8,7 +8,13 @@
 frutas = ('banana', 'maçã', 'pera')
 print(frutas) 
 
+## criando tupla com apenas 1 item (obrigatório colocar uma vírgula)
+carros = ('van',)
+print(carros)
 
 ## Verificando quantidade de elementos
 print(len(frutas))
+
+## type(tupla): verificar tipo do elemento
+print(type(carros))
 
