7132671d29d2bdcc45c3a40791dcb8ffbb47a005
yt <lucas_matias@protonmail.com>
2025-05-10 20:03:45 -0300

    Atualizando python estruturado: dicionario

--- python-estruturado/estudo_dicionarios.py
+++ python-estruturado/estudo_dicionarios.py
@@ -46,16 +46,50 @@ for valor in pessoa.values():
 print(pessoa.values(), type(pessoa.values()))
 
 # Adicionando elementos ao dicionário
+pessoas = dict(nome='Felipe', idade= 25, profissao= 'engenheiro')
+print(pessoas)
+pessoas['genero'] = 'masculino'
+print(pessoas)
 
-    
 # Métodos dicionários
+pessoas = dict(nome='Felipe', idade= 25, profissao= 'engenheiro')
 
-## dicionario.pop(chave): Remove um elemento do dicionário
+
+## dicionario.pop(chave): Remove um elemento do dicionário e o retorna
+print(pessoas.pop('nome'))
+print(pessoas)
+
+## dicionario.popitem(): Remove o último item inserido no dicionário; Versão 3.7 do python
+pessoas = dict(nome='Felipe', idade= 25, profissao= 'engenheiro')
+pessoas.popitem()
+print(pessoas)
 
 ## dicionario.keys(): Retorna um tipo dict_keys - iterável - com as chaves do dicionário
+pessoas = dict(nome='Felipe', idade= 25, profissao= 'engenheiro')
+print(pessoas.keys())
 
 ## dicionario.values(): Retorna um tipo dict_values - iterável - com os valores do dicionário
+print(pessoas.values())
 
-## dicionario.clear() : Remove todos os valores e chaves dos dicionários
+## dicionario.items(): Retorna um tipo_dict_items - iterável - com as chaves e os valores em uma tupla 
+print(pessoas.items())
 
-## 
+
+## dicionario.clear() : Remove todos os valores e chaves dos dicionários; Retorna um None
+pessoas = dict(nome='Felipe', idade= 25, profissao= 'engenheiro')
+pessoas.clear()
+print(pessoas)
+
+## dicionario.copy() : gerar um cópia do dicionário com outro endereço de memória
+pessoas = dict(nome='Felipe', idade= 25, profissao= 'engenheiro')
+pessoas2 = pessoas.copy()
+print(pessoas2) 
+
+## dicionario.update(dicionario) : Adiciona ao dicionário os pares de outro dicionário
+
+## dicionario.setdefault(): Retorna um valor associado a uma chave. Se a chave não existir, cria um par chave-valor com ela e com o valor de None;
+pessoas = dict(nome='Felipe', idade= 25, profissao= 'engenheiro')
+pessoas.setdefault('nome')
+print(pessoas)
+pessoas.setdefault(25)
+print(pessoas)
