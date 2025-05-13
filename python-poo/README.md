987a3b583779cb4946d24164168847aa872c89f0
yt <lucas_matias@protonmail.com>
2025-05-12 22:00:28 -0300

    Atualizacao: python POO

---
+++ python-poo/README.md
@@ -0,0 +1,7 @@
+# Descrever o conteúdo das aulas de POO
+
+## Aula 1 - Conceitos de POO
+Programação orientada a objetos é uma forma de representar uma entidade do mundo real, com suas propriedades, funções e características, por meio de um código que a modela.    
+
+
+## Aula 2 - Herança
---
+++ python-poo/estudo_classe_nokia.py
@@ -0,0 +1,35 @@
+'''
+    Representando um nokia
+'''
+
+
+class Celular:
+    # atributos
+    marca = 'Nokia'
+    modelo = 'Tijolão'
+    cor = 'azul'
+    tem_camera = False
+    bateria =  'infinta'
+    
+    # métodos da classe
+    def fazer_ligacoes(self):
+        print('Fazendo ligações...')
+        
+    def fazer_cobrinha(self):
+        print('Jogando cobrinha...')
+        
+    def despertador(self):
+        print('Despertando ...') 
+        
+    def calcular(self, v1, v2):
+        return v1+v2
+        
+
+# criando uma instancia de classe
+celular = Celular()
+
+
+print(celular) # mostra o endereço da classe
+print(celular.modelo) # mostrando propriedade
+celular.despertador() # executando função
+print(celular.calcular(2,3))
